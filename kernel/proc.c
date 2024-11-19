e2da4e6c58106cba146826b4e63d524c6b32d3d3
vivy <2686220852@qq.com>
2024-11-19 18:32:12 +0800

    change getpid

--- kernel/proc.c
+++ kernel/proc.c
@@ -211,8 +211,9 @@ proc_pagetable(struct proc *p)
 
 
   
-  if(mappages(pagetable, USYSCALL, PGSIZE, (uint64)usys, PTE_R | PTE_U) < 0){
-    uvmunmap(pagetable, USYSCALL, 1, 0);
+  if(mappages(pagetable, USYSCALL, PGSIZE, (uint64)p->usys, PTE_R | PTE_U) < 0){
+    uvmunmap(pagetable, TRAMPOLINE, 1, 0);
+    uvmunmap(pagetable, TRAPFRAME, 1, 0);
     uvmfree(pagetable, 0);
     return 0;
   }
